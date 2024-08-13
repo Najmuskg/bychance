6f58815b67a1274c371e5305c4e8015f25278c5e
Sakib <najmuskg@gmail.com>
2024-08-13 08:26:27 +0600

    ignore setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+
+      fontSize: {
+        22: "1.375rem",
+      },
+    },
+  },
+  plugins: [],
+};
